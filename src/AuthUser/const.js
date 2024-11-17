4fd95f07e5f8801ee82ebea3340cae596ad939c5
Serednii Mykola <seredniimykola@gmail.com>
2024-11-17 08:40:17 +0100

    change color Active

--- src/AuthUser/const.js
+++
@@ -1,8 +0,0 @@
-
-
-// export const BASE_URL = "http://localhost:7000";
-// export const BASE_URL = "https://jwtserver-production.up.railway.app"
-
-// export const URL_GRAPHQL = BASE_URL + "/api/graphql";
-// export const URL_AUTH = BASE_URL + "/api";
-
